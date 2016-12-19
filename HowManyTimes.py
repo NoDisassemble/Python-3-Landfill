f938789576c14b5d413b6668e3dbcb50210dab99
NoDisassemble <anything@dant35an0nym0u5.com>
2016-12-18 23:27:45 -0800

    Add files via upload

---
+++ HowManyTimes.py
@@ -0,0 +1,36 @@
+import time
+
+print('''
+--------------------------------------------------------------------------------
+A simple program that prints whatever you type for however many times you say.
+Dec 2016 v1.0
+by
+NoDisassemble.me
+--------------------------------------------------------------------------------
+''')
+
+while True:
+    what=input("What would you like to say? ")
+    try:
+        x=int(input("How many times to say it? "))
+        print("")
+        print("[+] Typing " + str(what) + " " + str(x) + " times [+]")
+        time.sleep(1)
+        while x != 0:
+            print(what)
+            x = x - 1
+        print("[!] Typing Completed [!]")
+        print("")
+        input("Press Enter to run again:")
+        print("")
+    except ValueError:
+        print("")
+        print("Invalid response:")
+        input("Press Enter to try again:")
+        print('')
+        continue
+
+
+
+
+
