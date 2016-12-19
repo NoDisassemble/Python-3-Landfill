bfcfc7e72323f724ab35c8591e15c007338e3b8b
NoDisassemble <anything@dant35an0nym0u5.com>
2016-12-18 23:18:11 -0800

    Add files via upload

---
+++ countTo.py
@@ -0,0 +1,29 @@
+import time
+
+print('''
+--------------------------------------------------------------------------
+A simple program that counts to whatever number you enter.
+Dec 2016 v1.0
+by
+NoDisassemble.me
+--------------------------------------------------------------------------
+''')
+
+while True:
+    try:
+        count=1
+        x=int(input("Enter a number to count to: "))
+        print("[+] Counting to " + str(x) + " [+]")
+        time.sleep(1)
+        while count<x+1:
+            print (count)
+            count=count+1
+        print("[!] Count Completed [!]")
+        print("")
+        input("Press Enter to run again:")
+        print("")
+    except ValueError:
+        print("")
+        print("Invalid response:")
+        input("Press enter to try again:")
+        print("")
